07c6a1c710678a17180ba9ef4fdfa786ab6ffa08
Ayush Kumar Singh <ayush.k@outlook.com>
2023-08-04 10:11:47 -0400

    Added restart functionality when ball hits side walls

--- src/app/game.c
+++ src/app/game.c
@@ -13,49 +13,57 @@
 
 // Function Prototype(s)
 void draw_mid_line(SDL_Renderer *renderer);
+bool hit_side_walls(SDL_Rect *ball);
 
 int game(SDL_Renderer *renderer) {
 	bool is_game_running = true;
 
-	const Uint8 *keyboard_state = SDL_GetKeyboardState(NULL);
-
-	// Initializing entities
-	SDL_Rect player;
-	init_paddle(&player, true);
-	SDL_Rect opponent;
-	init_paddle(&opponent, false);
-	SDL_Rect ball;
-	init_ball(&ball);
-	int x_direction = (time(NULL) % 2) ? 1 : -1;
-	int y_direction = (time(NULL) % 2) ? -1 : 1;
-
 	while (is_game_running) {
-		SDL_Event event;
+		const Uint8 *keyboard_state = SDL_GetKeyboardState(NULL);
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		// Initializing entities
+		SDL_Rect player;
+		init_paddle(&player, true);
+		SDL_Rect opponent;
+		init_paddle(&opponent, false);
+		SDL_Rect ball;
+		init_ball(&ball);
+		int x_direction = (time(NULL) % 2) ? 1 : -1;
+		int y_direction = (time(NULL) % 2) ? -1 : 1;
 
-		SDL_RenderClear(renderer);
+		while (1) {
+			SDL_Event event;
 
-		draw_mid_line(renderer);	
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-		// Draw entities
-		draw_paddle(renderer, &player, true);
-		draw_paddle(renderer, &opponent, false);
-		draw_ball(renderer, &ball);
+			SDL_RenderClear(renderer);
+
+			draw_mid_line(renderer);	
+
+			// Draw entities
+			draw_paddle(renderer, &player, true);
+			draw_paddle(renderer, &opponent, false);
+			draw_ball(renderer, &ball);
 		
-		// Updating entity movement
-		player_detect_movement(&player, keyboard_state);
-		move_ball(&ball, &x_direction, &y_direction, &player, &opponent);
-		move_opponent(&opponent, &ball);
+			// Updating entity movement
+			player_detect_movement(&player, keyboard_state);
+			move_ball(&ball, &x_direction, &y_direction, &player, &opponent);
+			move_opponent(&opponent, &ball);
 		
-		if (SDL_PollEvent(&event) > 0) {
-			if (event.type == SDL_QUIT) is_game_running = false;
+			if (hit_side_walls(&ball)) {
+				break;
+			}
+
+			if (SDL_PollEvent(&event) > 0) {
+				if (event.type == SDL_QUIT) {
+					is_game_running = false;
+					break;
+				}
+			}
+
+			SDL_RenderPresent(renderer);
 		}
-
-		SDL_RenderPresent(renderer);
 	}
-
-	SDL_SetRenderDrawColor(renderer, 1, 111, 185, 255);
 
 	return 0;
 }
@@ -71,4 +79,12 @@ void draw_mid_line(SDL_Renderer *renderer) {
 
 		SDL_RenderFillRect(renderer, &line);
 	}
+}
+
+bool hit_side_walls(SDL_Rect *ball) {
+	if ((*ball).x + (*ball).w == WINDOW_WIDTH || (*ball).x == 0) {
+		return true;
+	}
+
+	return false;
 }
